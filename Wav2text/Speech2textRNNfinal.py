5400995c0418106d28fa6d9b982ce0d66b6a652e
AntoineMsy <42070990+AntoineMsy@users.noreply.github.com>
2022-04-18 22:45:56 +0200

    Add files via upload

--- Wav2text/Speech2textRNNfinal.py
+++ Wav2text/Speech2textRNNfinal.py
@@ -267,13 +267,13 @@ class TextTransform:
 
 # torchaudio.transforms.MelSpectrogram(sample_rate=16000, n_mels=128),
 train_audio_transforms = nn.Sequential(
-    Add_noise(),
+    
     torchaudio.transforms.MelSpectrogram(sample_rate=16000, n_mels=128),
     torchaudio.transforms.FrequencyMasking(freq_mask_param=15),
     torchaudio.transforms.TimeMasking(time_mask_param=35)
 )
 
-valid_audio_transforms = torchaudio.transforms.MelSpectrogram()
+valid_audio_transforms = nn.sequential(Add_noise(),torchaudio.transforms.MelSpectrogram())
 
 text_transform = TextTransform(char_map_str)
 
