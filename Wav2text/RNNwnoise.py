566e412afd7fc599b1e597ada79580ea57f9dbf6
AntoineMsy <42070990+AntoineMsy@users.noreply.github.com>
2022-04-23 15:35:44 +0200

    Add files via upload

--- Wav2text/RNNwnoise.py
+++ Wav2text/RNNwnoise.py
@@ -260,7 +260,8 @@ class Add_real_noise(nn.Module):
     def __init__(self):
         super(Add_real_noise,self).__init__()
     def __call__(self, sample):
-        return add_real_noise(sample)
+        snr = np.random.choice([10,20,30])
+        return add_real_noise(sample,snr)
     
 class Add_noise(nn.Module):
     #Add noise at a random level between 0, -5 and 5 dB
@@ -521,7 +522,7 @@ def train(model, device, train_loader, criterion, optimizer, scheduler, epoch, i
             output = output.transpose(0, 1) # (time, batch, n_class)
           
             loss = criterion(output, labels, input_lengths, label_lengths)
-            
+            kl_loss = nn.KLDivLoss()
             out_n = model(noise_spectrograms)  # (batch, time, n_class)
             out_n = F.log_softmax(out_n, dim=2)
             
@@ -534,7 +535,7 @@ def train(model, device, train_loader, criterion, optimizer, scheduler, epoch, i
     
             loss_dn = criterion(output, labels, input_lengths, label_lengths)
             
-            total_loss = (loss+loss_n+loss_dn)/3
+            total_loss = loss+ (loss_n+loss_dn)/2 + kl_loss(out_n,output) + kl_loss(out_dn,output)
             total_loss.backward()
     
             experiment.log_metric('loss', total_loss.item(), step=iter_meter.get())
@@ -621,12 +622,12 @@ def main(experiment,learning_rate=5e-4, batch_size=8, epochs=20,
                                 shuffle=False,
                                 collate_fn=lambda x: data_processing(x, 'valid'),
                                 **kwargs)
-    #PATH = "deepspeech.pt"
+    PATH = "deepspeech.pt"
     model = SpeechRecognitionModel(
         hparams['n_cnn_layers'], hparams['n_rnn_layers'], hparams['rnn_dim'],
         hparams['n_class'], hparams['n_feats'], hparams['stride'], hparams['dropout']
         ).to(device)
-    #model.load_state_dict(torch.load(PATH, map_location = device))
+    model.load_state_dict(torch.load(PATH, map_location = device))
     
     print(next(iter(train_loader))[0][0][0].shape)
     #print(plot_spectrogram(next(iter(train_loader))[0][0][0]))
